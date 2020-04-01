cbafe2afe60ffb59adbd35ef1941d88d084e0cdf
Ian Stapleton Cordasco <icordasco@heroku.com>
2020-04-01 11:31:52 -0500

    Bump version to 2.0.2

--- src/pghstore/version.py
+++ src/pghstore/version.py
@@ -5,7 +5,7 @@ You can find the current version in the command line interface:
 
 .. sourcecode:: console
 
-   $ python -m wand.version
+   $ python -m pghstore.version
    0.9.2
 
 .. versionadded:: 1.0.0
@@ -13,17 +13,17 @@ You can find the current version in the command line interface:
 """
 from __future__ import print_function
 
-__all__ = 'VERSION', 'VERSION_INFO'
+__all__ = "VERSION", "VERSION_INFO"
 
 
 #: (:class:`tuple`) The version tuple e.g. ``(0, 9, 2)``.
-VERSION_INFO = (2, 0, 1)
+VERSION_INFO = (2, 0, 2)
 
 #: (:class:`six.string_types`) The version string e.g. ``'0.9.2'``.
-VERSION = '%d.%d.%d' % VERSION_INFO
+VERSION = "%d.%d.%d" % VERSION_INFO
 
-__doc__ = __doc__.replace('0.9.2', VERSION)
+__doc__ = __doc__.replace("0.9.2", VERSION)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(VERSION)
