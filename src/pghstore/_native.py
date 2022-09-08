7b1f7f1873f7161f6b2994e8918c1cd7f89a42d8
alouie-sfdc <arthur.louie@salesforce.com>
2022-09-07 23:00:48 -0700

    Apply black formatting changes

--- src/pghstore/_native.py
+++ src/pghstore/_native.py
@@ -177,7 +177,9 @@ def dump(obj, file, key_map=None, value_map=None, encoding="utf-8"):
             value = None
         elif not (
             isinstance(value, six.string_types)
-            or isinstance(value, six.binary_type)  # noqa W503 (this is how black formats it)
+            or isinstance(  # noqa W503 (this is how black formats it)
+                value, six.binary_type
+            )
         ):
             if value_map is None:
                 raise TypeError(
